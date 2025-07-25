c135cfc93a05b0cd61c46e12dcb3fae615880734
ghcit2025 <ghcit@goldenhillsindia.com>
2025-07-25 11:11:02 +0530

    added the basic setuo

--- project/src/main.tsx
+++ project/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
 import './index.css';
+import GhcMain from './components/Main/GhcMain.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <GhcMain />
   </StrictMode>
 );
